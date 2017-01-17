4dcedb2e42cbb41122e00b28480f5273f1d6d506
ulfdahlstrand <ulf.d.dahlstrand@gmail.com>
2017-01-17 21:39:44 +0100

    moved db connection to more central place

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ var App = require("ms-core");
 var config = require("./config.js");
 App.init(config);
 
+require("./db.js");
 require("./recruit/routes")();
--- recruit/recruits.js
+++ recruit/recruits.js
@@ -1,19 +1,9 @@
-var config = require("./../config");
-var mongoose = require('mongoose');
 var Recruit = require("./Recruit");
 var helpers = require("./helpers");
 const url = require('url');
 var when = require('when');
 function RecruitService() {
 	var self = this;
-
-	mongoose.connect(config.db_connectionString);
-
-	var db = mongoose.connection;
-	db.on('error', console.error.bind(console, 'connection error:'));
-	db.once('open', function() {
-	  // we're connected!
-	});
 
 	self.getAll = function(){
 		var deferred = when.defer();
